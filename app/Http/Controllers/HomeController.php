37b9dfd162bff8d877a1fa3212fa695c3aa43ea9
Aminul Islam <nullghost13@gmail.com>
2017-06-29 14:48:30 +0600

    Initial Commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        if(auth()->check()){
+        $this->middleware('auth:admin');
+        }
+        else if(Auth::guard('web')->check()){
+        $this->middleware('auth:web');
+        }else{
+            $this->middleware('auth:web');
+        }
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
